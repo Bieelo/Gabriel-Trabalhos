d95e098b4dc6999edcc1cb867910fea6183da4f5
Bieelo <gabrielferreira15436@outlook.com>
2022-01-07 17:25:56 -0300

    cesharpe

---
+++ C#/Condicionais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Variaveis 
+{
+    static void Main()
+    {
+    
+    int a = 80;
+    int b = 100;
+    int c = (a + b);
+    
+    
+    if (c >= 100) //se a variavel C é maior ou igual a 31
+    {
+    Console.WriteLine("É verdade");
+    }
+
+    else if (c < 10)
+    {
+    Console.WriteLine("Você é idiota por a caso?");
+    }
+
+
+    else //senão 
+    {
+    Console.WriteLine("É mentira");
+    }
+    
+    
+    Console.ReadLine();
+
+    }
+}
