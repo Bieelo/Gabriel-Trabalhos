cfc0d4a9302ef8f796bd95469a6cec5c0b24ec9f
Bieelo <gabrielferreira15436@outlook.com>
2022-01-07 12:35:24 -0300

    Pratica do Cesharpe

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class funcaonome
+{
+    static void Main()
+    {
+       Console.WriteLine("Escreva seu nome: ");
+       string Nome = Console.ReadLine();
+
+
+
+       Console.WriteLine("Seu nome é");
+       Console.WriteLine(Nome);
+            
+
+        Console.ReadLine();
+    }
+}
