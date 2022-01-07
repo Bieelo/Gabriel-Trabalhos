98133b42a3dc99080da228da7e4c788cadb26f55
Bieelo <gabrielferreira15436@outlook.com>
2022-01-07 18:58:20 -0300

    Operadores lógicos

---
+++ C#/Operadores logicos/Anotações para Revisão.md
@@ -0,0 +1,4 @@
+# Anotações para Revisão 
+
+Converter texto para número: Int.Parse
+Exemplo: Int idade = Int.Parse(Console.Readline());
---
+++ C#/Operadores logicos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{ 
+    public static void Main(string[] args)
+        {
+           Console.WriteLine("Digite seu nome: ");
+           string nome = Console.ReadLine();
+
+           Console.WriteLine("Digite sua nota");
+           float nota1 = float.Parse(Console.ReadLine());
+
+           Console.WriteLine("Digite sua nota");
+           float nota2 = float.Parse(Console.ReadLine());
+
+           Console.WriteLine("Digite sua nota");
+           float nota3 = float.Parse(Console.ReadLine());
+
+           Console.WriteLine("Digite sua nota");
+           float nota4 = float.Parse(Console.ReadLine());
+
+           float media = (nota1+nota2+nota3+nota4)/4; 
+
+           Console.WriteLine( nome +", sua média é de: "+ media);
+
+           Console.ReadLine();
+
+        }
+}
